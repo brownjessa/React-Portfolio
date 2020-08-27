c5436ae73f435b66e6eb5747ba3ede0132e41640
Jessamine Brown <brownjessa11@gmail.com>
2020-08-27 14:48:34 -0500

    set up links for home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 class App extends React.Component {
   constructor (props){
     super(props);
+    this.state = {
+      title: 'Jessamine Brown',
+      headerLinks: [
+        { title: 'Home', paht: '/' },
+        { title: 'About', path: '/about'},
+        { title: 'Contact', path: '/contact' }
+      ],
+      
+    }
 
   }
 
