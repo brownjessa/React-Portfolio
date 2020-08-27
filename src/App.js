5431e99fa4e64f8f2f20d12a8e0e03e3291b7b0c
Jessamine Brown <brownjessa11@gmail.com>
2020-08-27 15:41:47 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 
 class App extends React.Component {
@@ -27,7 +29,29 @@ class App extends React.Component {
   }
 
   render() {
-    return <div> Hello From React </div>;
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}></Container>
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+          <Navbar.Brand>Jessamine Brown</Navbar.Brand>
+
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Router>
+    );
   }
 }
 
