2e9ebd7b2473e70f864daf4851a634947d60d1e0
Jessamine Brown <brownjessa11@gmail.com>
2020-08-27 15:42:24 -0500

    import nav from react bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 class App extends React.Component {
