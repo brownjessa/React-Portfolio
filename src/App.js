46f45a0f7f5bb07efd89c28f5cd46955bef09363
Jessamine Brown <brownjessa11@gmail.com>
2020-08-27 15:01:23 -0500

    parsing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
-  constructor (props){
+  constructor(props) {
     super(props);
     this.state = {
-      title: 'Jessamine Brown',
+      title: "Jessamine Brown",
       headerLinks: [
-        { title: 'Home', paht: '/' },
-        { title: 'About', path: '/about'},
-        { title: 'Contact', path: '/contact' }
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
       ],
       home: {
-        title: 'Be Determined'
-        subTitle: 'Projects that inspire'
-        text: 'Checkout my projects below'
+        title: "Be Determined",
+        subTitle: "Projects that inspire",
+        text: "Checkout my projects below",
       },
       about: {
-        title: 'About Me'
+        title: "About Me",
       },
       contact: {
-        title: 'Let\'s Talk'
-      }
-    }
-
+        title: "Let's Talk",
+      },
+    };
   }
 
-  render (
-    return (
-      <div> Hello From React </div>
-
-    );
-  )
+  render() {
+    return <div> Hello From React </div>;
+  }
 }
 
 export default App;
